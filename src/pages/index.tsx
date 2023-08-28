a878cb398c5cf93a325d50129d5f9d6f4a8e2e02
ScalarZ <faresrahali44@gmail.com>
2023-08-28 07:00:13 +0100

    removing session props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,8 +19,6 @@ export default function Home({ session }: { session: Session }) {
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getServerSession(context.req, context.res, authOptions);
-  const user = getSessionUser(session);
   return {
     props: {},
   };
