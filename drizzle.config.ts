1aeb04d0921bcb6a2585000c54d4f8cb43770e89
ScalarZ <faresrahali44@gmail.com>
2023-11-04 22:45:53 +0100

    fixing drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,10 +5,7 @@ export default {
   schema: "./src/schema.ts",
   out: "./drizzle",
   dbCredentials: {
-    database: "postgres",
-    host: "db.mnlgmybxfwugjaizllrn.supabase.co",
-    password: "nDRd2bPaiEcWOOP9",
-    port: 5432,
-    user: "postgres",
+    connectionString:
+      "postgresql://postgres:3B7c44y0qjWqMUQO@db.mnlgmybxfwugjaizllrn.supabase.co:5432/postgres",
   },
 } satisfies Config;
